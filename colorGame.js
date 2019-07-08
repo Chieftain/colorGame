6dd50a0cbf4c06c6b14912cc4153e561ef5e320a
Chieftain <30796309+Chieftain@users.noreply.github.com>
2019-07-08 16:45:10 +0300

    Update colorGame.js

--- colorGame.js
+++ colorGame.js
@@ -6,5 +6,30 @@ var colors = [
     "rgb(0, 255, 255)",
     "rgb(0, 0, 255)",
 ];
-var squares = document.querySelectorAll(".square")
+var squares = document.querySelectorAll(".square");
+var colorDisplay = document.querySelector("#colorDisplay");
+var displayMessage = document.querySelector("#message");
+var pickedColor = colors[3];
 
+colorDisplay.textContent = pickedColor;
+
+for (var i = 0; i < squares.length; i++) {
+
+    squares[i].style.backgroundColor = colors[i];
+
+    squares[i].addEventListener('click', function(){
+        var clickedColor = this.style.backgroundColor;
+        if (clickedColor === pickedColor){
+            displayMessage.textContent = "Correct";
+            changeColors(clickedColor);
+        } else {
+            this.style.backgroundColor = "#232323";
+            displayMessage.textContent = "Try again";
+        }
+    })
+}
+function changeColors (color){
+    for (var i = 0; i < squares.length; i++){
+        squares[i].style.backgroundColor = color;
+    }
+}
